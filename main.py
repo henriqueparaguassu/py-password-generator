85de6a368b3fcbb7e9330b4286e939f561593f50
Henrique Paraguassu <henriquevoglerparaguassu@gmail.com>
2022-02-24 16:00:22 -0300

    added script description; improved error handling

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+"""
+Author: Henrique M. Paraguassu
+Email: henriquevoglerparaguassu@gmail.com
+Github: henriqueparaguassu
+
+This program will generate a password with the given parameters given 
+by the user from the command line. The parameters are:
+    - length: the length of the password
+    - uppercase: if the password will have uppercase
+    - lowercase: if the password will have lowercase
+    - number: if the password will have numbers
+    - symbol: if the password will have symbols
+
+The script can catch the following exceptions:
+    - ValueError: if the length is not an integer
+    - IndexError: if the length not passed from the command line
+    - KeybordInterrupt: if the user press Ctrl+C
+
+This script can be run from the command line with the following command:
+    $ python main.py 10
+    Where 10 is the length of the password
+
+main.py use the following libraries:
+    - random
+    - sys
+
+Formatted with black
+"""
+
 import random
 import sys
 
@@ -60,27 +89,60 @@ options = []
 
 # Get the length of the password from the command line, default is 8
 try:
-    length = sys.argv[1]
-    if length.isdigit():
-        length = int(length)
-    else:
-        raise TypeError("The length must be a number")
-except IndexError or TypeError:
-    print("Not enough parameters, using default length (8)")
-    length = 8
+    print("\t" + "#" * 41)
+    print("\t##  Welcome to the password generator  ##")
+    print("\t" + "#" * 41)
+    print("\n")
 
-# Get the parameters from the user
-options.append(
-    False if input("Do you want uppercase characteres? (y/n) ['y']: ") == "n" else True
-)
-options.append(
-    False if input("Do you want lowercase characteres? (y/n) ['y']: ") == "n" else True
-)
-options.append(False if input("Do you want numbers? (y/n) ['y']: ") == "n" else True)
-options.append(False if input("Do you want symbols? (y/n) ['y']: ") == "n" else True)
-print()
+    try:
+        length = sys.argv[1]
+        if length.isdigit():
+            length = int(length)
+        else:
+            raise ValueError
 
-passwd = generate_passoword(length, *options)
-print(passwd) if passwd else sys.exit()
+    except IndexError:
+        try:
+            length = input("How long do you want your password? (default is 8): ")
+            if length.isdigit():
+                length = int(length)
+            else:
+                raise ValueError("The length must be a number")
 
-sys.exit()
+        except ValueError:
+            print("No length was given, using 8 as default\n")
+            length = 8
+
+    except ValueError:
+        print("No length was given, using 8 as default\n")
+        length = 8
+
+    # Get the parameters from the user
+    options.append(
+        False
+        if input("Do you want uppercase characteres? (y/n) ['y']: ") == "n"
+        else True
+    )
+    options.append(
+        False
+        if input("Do you want lowercase characteres? (y/n) ['y']: ") == "n"
+        else True
+    )
+    options.append(
+        False if input("Do you want numbers? (y/n) ['y']: ") == "n" else True
+    )
+    options.append(
+        False if input("Do you want symbols? (y/n) ['y']: ") == "n" else True
+    )
+    print()
+
+    passwd = generate_passoword(length, *options)
+    print(
+        f'Password generated: "{passwd}" (without double quotes)'
+    ) if passwd else sys.exit()
+
+    sys.exit()
+
+except KeyboardInterrupt:
+    print("\nExiting...")
+    sys.exit()
